8ed984634d9ebfe8fa7d0a00fc21f365d61351f6
Nicholas Rodine <halfofastaple@gmail.com>
2025-06-27 06:14:02 -0500

    ported over model globals

--- src/Model.h
+++ src/Model.h
@@ -44,35 +44,4 @@ const std::size_t ResultsDoubleOffsetStart = offsetof(Results, S) / sizeof(doubl
 
 #pragma pack(pop)
 
-extern Person* Hosts;
-extern std::vector<PersonQuarantine> HostsQuarantine;
-extern Household* Households;
-extern PopVar State, StateT[MAX_NUM_THREADS];
-extern Cell* Cells, ** CellLookup;
-extern Microcell* Mcells, ** McellLookup;
-extern std::vector<uint16_t> mcell_country;
-extern Place** Places;
-extern AdminUnit AdUnits[MAX_ADUNITS];
-
-//// Time Series defs:
-//// TimeSeries is an array of type results, used to store (unsurprisingly) a time series of every quantity in results. Mostly used in RecordSample.
-//// TSMeanNE and TSVarNE are the mean and variance of non-extinct time series. TSMeanE and TSVarE are the mean and variance of extinct time series. TSMean and TSVar are pointers that point to either extinct or non-extinct.
-extern Results* TimeSeries, *TSMean, *TSVar, *TSMeanNE, *TSVarNE, *TSMeanE, *TSVarE; //// TimeSeries used in RecordSample, RecordInfTypes, SaveResults. TSMean and TSVar
-
-extern Airport* Airports;
-extern Events* InfEventLog;
-extern int nEvents;
-
-
-extern double inftype[INFECT_TYPE_MASK], inftype_av[INFECT_TYPE_MASK], infcountry[MAX_COUNTRIES], infcountry_av[MAX_COUNTRIES], infcountry_num[MAX_COUNTRIES];
-extern double indivR0[MAX_SEC_REC][MAX_GEN_REC], indivR0_av[MAX_SEC_REC][MAX_GEN_REC];
-extern double inf_household[MAX_HOUSEHOLD_SIZE + 1][MAX_HOUSEHOLD_SIZE + 1], denom_household[MAX_HOUSEHOLD_SIZE + 1];
-extern double inf_household_av[MAX_HOUSEHOLD_SIZE + 1][MAX_HOUSEHOLD_SIZE + 1], AgeDist[NUM_AGE_GROUPS], AgeDist2[NUM_AGE_GROUPS];
-extern double case_household[MAX_HOUSEHOLD_SIZE + 1][MAX_HOUSEHOLD_SIZE + 1], case_household_av[MAX_HOUSEHOLD_SIZE + 1][MAX_HOUSEHOLD_SIZE + 1];
-extern double PropPlaces[NUM_AGE_GROUPS * AGE_GROUP_WIDTH][MAX_NUM_PLACE_TYPES];
-extern double PropPlacesC[NUM_AGE_GROUPS * AGE_GROUP_WIDTH][MAX_NUM_PLACE_TYPES], AirTravelDist[MAX_DIST];
-extern double PeakHeightSum, PeakHeightSS, PeakTimeSum, PeakTimeSS;
-
-extern int DoInitUpdateProbs;
-
 #endif // COVIDSIM_MODEL_H_INCLUDED_
