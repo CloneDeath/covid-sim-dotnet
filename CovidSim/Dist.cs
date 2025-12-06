afe11784cb6cc5a6b3bb98759285a24b7186bdc8
Nicholas Rodine <halfofastaple@gmail.com>
2025-12-06 07:41:42 -0500

    moved over dist2_mm

--- CovidSim/Dist.cs
+++ CovidSim/Dist.cs
@@ -122,6 +122,23 @@ public static class Dist {
 		return periodic_xy(x, y);
 	}
 
+	public static double dist2_mm(Microcell a, Microcell b)
+	{
+		double x, y;
+		int l, m;
+
+		l = (int)(a - Mcells);
+		m = (int)(b - Mcells);
+		if (P.DoUTM_coords)
+		{
+			return dist2UTM(P.in_microcells_.width * fabs((double)(l / P.total_microcells_high_)), P.in_microcells_.height * fabs((double)(l % P.total_microcells_high_)),
+				P.in_microcells_.width * fabs((double)(m / P.total_microcells_high_)), P.in_microcells_.height * fabs((double)(m % P.total_microcells_high_)));
+		}
+		x = P.in_microcells_.width * fabs((double)(l / P.total_microcells_high_ - m / P.total_microcells_high_));
+		y = P.in_microcells_.height * fabs((double)(l % P.total_microcells_high_ - m % P.total_microcells_high_));
+		return periodic_xy(x, y);
+	}
+
 	public static double periodic_xy(double x, double y) {
 		if (Param.P.DoPeriodicBoundaries)
 		{
