af3299abfb1f7e295a030fd2e593d08e61d3d2c8
Nicholas Rodine <halfofastaple@gmail.com>
2025-12-08 05:30:07 -0500

    fixed some compilation issues

--- CovidSim/InverseCdf.cs
+++ CovidSim/InverseCdf.cs
@@ -1,7 +1,7 @@
 namespace CovidSim;
 
 public class InverseCdf {
-	private double cdf_values_[CDF_RES + 1];
+	private readonly double[] cdf_values_ = new double[Constants.CDF_RES + 1];
 
 	/**
 	 * Function: set_neg_log
@@ -11,9 +11,9 @@ public class InverseCdf {
 	 * @return void
 	 */
 	public void set_neg_log(double start_value) {
-		cdf_values_[CDF_RES] = start_value;
-		for (int i = 0; i < CDF_RES; i++)
-			cdf_values_[i] = -log(1 - ((double)i) / CDF_RES);
+		cdf_values_[Constants.CDF_RES] = start_value;
+		for (int i = 0; i < Constants.CDF_RES; i++)
+			cdf_values_[i] = -log(1 - ((double)i) / Constants.CDF_RES);
 	}
 
 	/**
@@ -23,8 +23,7 @@ public class InverseCdf {
 	 * @return void
 	 */
 	public void assign_exponent() {
-		for (int quantile = 0; quantile <= CDF_RES; quantile++)
-		{
+		for (int quantile = 0; quantile <= Constants.CDF_RES; quantile++) {
 			cdf_values_[quantile] = exp(-cdf_values_[quantile]);
 		}
 	}
@@ -37,8 +36,7 @@ public class InverseCdf {
 	 * @return void
 	 */
 	public void assign_exponent(double value) {
-		for (int i = 0; i <= CDF_RES; i++)
-		{
+		for (int i = 0; i <= Constants.CDF_RES; i++) {
 			cdf_values_[i] = exp(value);
 		}
 	}
@@ -53,27 +51,22 @@ public class InverseCdf {
 	 * @return value chosen from InverseCdf data
 	 */
 	public ushort choose(double Mean, int tn, double timesteps_per_day) {
-		unsigned short int Value;
-		int i;
-		double q, ti;
+		double q;
 
-		i = (int)floor(q = ranf_mt(tn) * CDF_RES); //// note q defined here as well as i.
+		var i = (int)floor(q = ranf_mt(tn) * CDF_RES); //// note q defined here as well as i.
 		q -= ((double)i); //// remainder
 
 		//// weighted average (sort of) between quartile values from CDF_RES. logged as it was previously exponentiated in ReadParams. Minus as exp(-cdf) was done in ReadParaams. Sort of
-		ti = -Mean * log(q * cdf_values_[i + 1] + (1.0 - q) * cdf_values_[i]);
-		Value = (unsigned short int) floor(0.5 + (ti * timesteps_per_day));
+		var ti = -Mean * log(q * cdf_values_[i + 1] + (1.0 - q) * cdf_values_[i]);
+		ushort Value = (ushort)floor(0.5 + (ti * timesteps_per_day));
 
 		return Value;
 	}
 
-	public double* get_values()
-	{
+	public double[] get_values() {
 		return cdf_values_;
 	}
 
 	// Overloading [] operator to access elements in array style
-	public double& operator[](int i) {
-		return cdf_values_[i];
-	}
+	public double this[int i] => cdf_values_[i];
 }
